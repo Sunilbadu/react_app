27ab1b66d2ce317eb0c6a61a434d6feeb2ecf0b5
Sunilbadu <sunylbadu7@gmail.com>
2026-06-21 21:19:23 +0545

    key mapping

--- src/nap.jsx
+++ src/nap.jsx
@@ -1,12 +1,32 @@
+// function Mapy(){
+//     const students= ["ram" , "shyam" , "hari"]
+//     return(
+//         <>
+//         {
+//             students.map((student)=>{
+//                return <h1 key={student}>hello {student}</h1>
+//             })
+//         }
+//         </>
+//     )
+// }
+// export default Mapy;
 function Mapy(){
-    const students= ["ram" , "shyam" , "hari"]
+    const students=[
+        {id:1, name: "sunil" , age:20},
+        {id:3, name: "sujeet" , age:22},
+        {id:6, name: "anish" , age:24},
+    ];
     return(
         <>
-        {
-            students.map((student)=>{
-               return <h1 key={student}>hello {student}</h1>
-            })
-        }
+              { students.map((students)=>{
+               return( <div key={students.id}>
+                    <h2>{students.name}</h2>
+                </div>)
+              }
+           
+        )}
+    
         </>
     )
 }
