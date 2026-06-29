a36dbbc21ff6078c7f1b7b1f14d551f2e69ef685
Sunilbadu <sunylbadu7@gmail.com>
2026-06-29 22:42:55 +0545

    timer

--- src/counter.jsx
+++ src/counter.jsx
@@ -12,4 +12,48 @@ function Counter (){
         </>
     )
 }
+import { useState, useEffect } from "react";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Timer</h1>
+
+      <h2>{time} seconds</h2>
+
+      <button onClick={() => setRunning(true)}>Start</button>
+
+      <button onClick={() => setRunning(false)} style={{ marginLeft: "10px" }}>
+        Stop
+      </button>
+
+      <button
+        onClick={() => {
+          setRunning(false);
+          setTime(0);
+        }}
+        style={{ marginLeft: "10px" }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
 export default Counter;
